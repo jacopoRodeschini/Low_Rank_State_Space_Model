d943d34a922e0bd4b150891d20f59825237b8ec3
j.rodeschini <j.rodeschini@studenti.unibg.it>
2025-08-28 16:43:28 +0200

    add the import from the covariance model

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,5 @@
 from .filter_jax import filter_smw_nan, smoother_smw, computeExpectedValues 
 
 from .utils import buildDataset, laplacian_smoothing, fit, buildBasis_list, buildH_dense, predict
+
+from .covariance_model import spdeAppoxCov, cov2corr, KLdivergence2Q
